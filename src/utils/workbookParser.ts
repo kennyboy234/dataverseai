dc32c1d7d1cd12b19932be150d563e0621812853
kennyboy234 <oloyedekehinde2002@gmail.com>
2026-09-25 08:42:56 +0100

    wip: workspace state before crash debug

--- src/utils/workbookParser.ts
+++ src/utils/workbookParser.ts
@@ -251,6 +251,8 @@ const workbookFromSheets = (
     return sheetFromMatrix(matrix, name, id, index, mergedRanges);
   });
 
+  console.log("[DataVerse DEBUG] SheetNames from XLSX.read:", workbook.SheetNames.length, workbook.SheetNames);
+
   if (sheets.length === 0) {
     throw new Error(`${fileName} does not contain a worksheet.`);
   }
